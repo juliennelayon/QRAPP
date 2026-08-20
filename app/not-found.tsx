d6a4e432e5a7e3ee75c31d438e227271bb2129f5
juliennelayon <layonjuliennejakosalem@gmail.com>
2026-08-20 14:59:06 +0800

    added module 1, 2, 3 and 4

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Not Found" }} />
+
+      <View style={styles.container}>
+        <Text>Page Not Found</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ constants/color.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: "#1E67D1",
+  },
+  dark: {
+    text: "#fff",
+    background: "#000",
+    tint: "#fff",
+  },
+};
